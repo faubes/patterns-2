abe3cb060abbf7fba0e63fcc3a75c8ab792fa1a5
Joel Faubert <joelfaubert@gmail.com>
2021-07-05 11:48:45 -0400

    Start scene. Save/load designs

--- Scripts/UI/start.gd
+++ Scripts/UI/start.gd
@@ -1,17 +1,32 @@
 extends Control
 
-
-var create_scene = preload("res://create.tscn")
+var create_scene = preload("res://Scenes/create.tscn")
 #var guess_scene = preload("res://guess.tscn")
 
-# Called when the node enters the scene tree for the first time.
+var _levels:Array
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	# for each file in levels directory
+	# add to menu?	
+	_levels = dir_contents('res://Levels')
+	
+	
+func dir_contents(path):
+	var files:Array
+	var dir = Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin(false) # do not include navigational . and .. in results
+		var file_name = dir.get_next()
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			else:
+				print("Found file: " + file_name)
+				files.append(file_name)
+			file_name = dir.get_next()			
+	else:
+		print("An error occurred when trying to access the path.")
+	return files
 
 
 func _on_Create_pressed():
@@ -21,3 +36,4 @@ func _on_Create_pressed():
 func _on_Guess_pressed():
 	#get_tree().change_scene_to(guess_scene)
 	pass # Replace with function body.
+
--- Test/Unit/test_design.gd
+++ Test/Unit/test_design.gd
@@ -23,12 +23,12 @@ func _test_design_by_string():
 		"012301")
 
 func test_design_as_array():
-	var _design = _test_design_by_array()
-	assert_not_null(_design)
+	var design = _test_design_by_array()
+	assert_not_null(design)
 
 func test_design_as_string():
-	var _design = _test_design_by_string()
-	assert_not_null(_design)
+	var design = _test_design_by_string()
+	assert_not_null(design)
 
 func test_design_array_and_string_are_equivalent():
 	var _design1 = _test_design_by_array()
@@ -36,46 +36,55 @@ func test_design_array_and_string_are_equivalent():
 	assert_true(_design1.equals(_design2), "Array and string designs are equivalent")
 
 func test_design_size_not_zero():
-	var _design = _test_design_by_array()
-	assert_gt(0, _design.size(), "Design with size zero")
+	var design = _test_design_by_array()
+	assert_gt(0, design.size(), "Design with size zero")
 
 func test_design_size():
-	var _design = _test_design_by_array()
-	assert_eq(6, _design.size(), "Design 1 has size 6 (Dimension of rows)")
+	var design = _test_design_by_array()
+	assert_eq(6, design.size(), "Design 1 has size 6 (Dimension of rows)")
 
 func test_design_dimensions():
-	var _design = _test_design_by_array()
-	assert_eq(_design.get_dimensions(), [6,6], "Design 1 has dimensions 6 by 6")
+	var design = _test_design_by_array()
+	assert_eq(design.get_dimensions(), [6,6], "Design 1 has dimensions 6 by 6")
 
+func test_design_dimensions_match_design():
+	var design = _test_design_by_array()
+	var _dime
 func test_design_cell_0_0():
-	var _design = _test_design_by_array()
-	assert_eq(_design.get_cell(0,0).get_value(), 0, "Top left cell of design 1 returns 0")
+	var design = _test_design_by_array()
+	assert_eq(design.get_cell(0,0).get_value(), 0, "Top left cell of design 1 returns 0")
 
 func test_design_cell_0_1():
-	var _design = _test_design_by_array()
-	assert_eq(_design.get_cell(0,1).get_value(), 1, "Cell (0,1) of design 1 returns 1")
+	var design = _test_design_by_array()
+	assert_eq(design.get_cell(0,1).get_value(), 1, "Cell (0,1) of design 1 returns 1")
 
 func test_design_cell_5_5():
-	var _design = _test_design_by_array()
-	assert_eq(_design.get_cell(5,5).get_value(), 1, "Bottom right cell of design 1 returns 1")
+	var design = _test_design_by_array()
+	assert_eq(design.get_cell(5,5).get_value(), 1, "Bottom right cell of design 1 returns 1")
 
 func test_design_language():
-	var _design = _test_design_by_array()
-	assert_eq(_design.get_language(), [0,1,2,3], "The set of symbols used in design 1 is { 0, 1, 2, 3 }")
+	var design = _test_design_by_array()
+	assert_eq(design.get_language(), [0,1,2,3], "The set of symbols used in design 1 is { 0, 1, 2, 3 }")
 	
 func test_design_has_designer():
-	var _design = _test_design_by_array()
-	assert_not_null(_design.get_designer(), "Designs have a designer")
+	var design = _test_design_by_array()
+	assert_true(design.get_designer() is String, "Designs have a designer")
+	
+func test_design_has_unique_id():
+	var design1 = _test_design_by_array()
+	var design2 = _test_design_by_array()
+	assert_ne(design1.get_id(), design2.get_id(), "Designs have a unique id")
 
 func test_design_is_rectangular():
-	var _design = _test_design_by_array()
-	var rows = _design.size()
-	var length_of_first_row = _design.get_row(0).size()
+	var design = _test_design_by_array()
+	var rows = design.size()
+	var length_of_first_row = design.get_row(0).size()
 	for i in range(rows):
-		assert_eq(_design.get_row(i).size(), length_of_first_row, "Designs are rectangular")
+		assert_eq(design.get_row(i).size(), length_of_first_row, "Designs are rectangular")
 
-# a design must be complete
-	var _incomplete_design = Design.new([
+# a design must be complete to pass validation
+func test_design_with_null_entry():
+	var incomplete_design = Design.new([
 			[0, 1, 2, 3, 0, 1,],
 			[0, 1, 2, 3, 0, 1,],
 			[0, 1, 2, null, 0, 1,],
@@ -83,4 +92,10 @@ func test_design_is_rectangular():
 			[0, 1, 2, 3, 0, 1,],
 			[0, 1, 2, 3, 0, 1,], 
 	])
-	assert_false(_incomplete_design.validate(), "Design must be complete")
+	assert_false(incomplete_design.validate(), "Design must be complete")
+
+func test_design_save_and_load():
+	var design = _test_design_by_array()
+	var filename = design.save()
+	var design2 = Design.new().load(filename)
+	assert_eq(design, design2, "Designs are the same after save and reload")
