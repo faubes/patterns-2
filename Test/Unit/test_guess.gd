bcdbd5d09d7b0145cc433fe01175817cc64fd0d9
Joel Faubert <joelfaubert@gmail.com>
2021-07-05 19:42:16 -0400

    cleanup guess tess

--- Test/Unit/test_guess.gd
+++ Test/Unit/test_guess.gd
@@ -3,8 +3,6 @@ extends 'res://addons/gut/test.gd'
 var Guess = load('res://Scripts/Core/guess.gd')
 var Design = load('res://Scripts/Core/design.gd')
 
-var design:Design
-var guess:Guess
 
 static func _test_design():
 	return Design.new(
@@ -17,39 +15,41 @@ static func _test_design():
 			[65, 66, 67, 68, 65, 66,],
 		])
 
-func _init():
-	design = _test_design()
-	guess = Guess.new(design)
-
 # guess matches dimensions of design
 func test_guess_dimension_same_as_design():
-	_init()
+	var design = _test_design()
+	var guess = Guess.new(design)
 	assert_eq(guess.get_dimensions(), design.get_dimensions(), "Guess and design have same dimensions")
 
 # guesser can request correct symbol from design
 func test_guess_request_cell():
-	_init()
+	var design = _test_design()
+	var guess = Guess.new(design)
 	assert_eq(guess.probe([0,0]), "A", "Guesser can sample symbols from design")
 
 # guesser cannot guess on a requested cell
 func test_guess_cannot_guess_requested_cell():
-	_init()
+	var design = _test_design()
+	var guess = Guess.new(design)
 	guess.probe([0,0])
 	assert_false(guess.guess([0,0], 1), "Guesser cannot guess a probed cell")
 
 # guess can be incomplete; need to request or fill every cell
 func test_guess_empty_incomplete():
-	_init()
+	var design = _test_design()
+	var guess = Guess.new(design)
 	guess.get_cell(0,0).value = null
 	assert_false(guess.validate(), "Empty guess is not valid")	
 	
 func test_guess_partial_incomplete():
-	_init()
+	var design = _test_design()
+	var guess = Guess.new(design)
 	guess.guess([0,0], 1) # guess that cell 0,0 has symbol 1
 	assert_false(guess.validate(), "Partial guess is not valid")
 
 func test_guess_incomplete():
-	_init()
+	var design = _test_design()
+	var guess = Guess.new(design)
 	guess.load_guess_array([
 			[null, "B", "C", "D", "A", "B",],
 			["A", "B", "C", "D", "A", "B",],
@@ -64,6 +64,7 @@ func test_guess_incomplete():
 
 # a complete guess can be validated and scored
 func test_guess_complete():
+	var design = _test_design()
 	var _complete_guess = Guess.new(design)
 	_complete_guess.load_guess_array([
 			["A", "B", "C", "D", "A", "B",],
